b4795b34f3e7c8e1b713992375ee4fa7bdcc0fde
abe87654321 <abe87654321@users.noreply.github.com>
2026-07-26 17:45:35 -0400

    fix: simplify jest config and add tsconfig for ts-jest

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,12 +1,10 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   testEnvironment: 'node',
-  rootDir: __dirname,
-  roots: ['<rootDir>'],
+  rootDir: '.',
   testMatch: ['**/*.test.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/packages/'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.ts$': 'ts-jest',
   },
   moduleNameMapper: {
     '^@sse/(.*)$': '<rootDir>/../packages/$1/src',
